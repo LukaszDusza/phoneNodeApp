ac2f666d5611d9d28554030d63b510ea76580f78
Lukasz Dusza <lukaszdusza280@gmail.com>
2019-01-12 18:12:07 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'phone-node-app';
+
+export class AppComponent implements OnInit{
+
+
+  
+  constructor(private http: HttpClient) { }
+  
+  title = 'Hello world ';
+  contacts = new Array<Contacts>();
+  
+  ngOnInit(): void {
+    this.getContacts();
+
+  }
+
+  getContacts() {
+    this.contacts = [];
+
+    this.http.get<Array<Contacts>>("http://localhost:8080/api/dto/contacts")
+      .subscribe(r => {
+        r.map(res => this.contacts.push(res));
+      }, err => {
+        //gdy napotka blad
+      }, () => {
+        console.log(this.contacts);
+        //odpala sie gdy skonczy sie subscribe
+      })
+  }
 }
+
+
+
+export class Contacts {
+  name: string;
+  surname: string;
+  number: number;
+}
+
+
